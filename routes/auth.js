406a071b0078ca38e3547b41287bfad788546278
Azeem Rasydan <azeemrasydan@gmail.com>
2021-05-09 13:11:01 +0800

    make it better

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-//------------ Importing Controllers ------------//
+// ------------ Importing Controllers ------------//
 const authController = require('../controllers/authController')
 
 //------------ Login Route ------------//
